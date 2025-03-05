e03cb5c2258278d147eda777d8bd759389480a96
Loan-Matarise <loanmata58@gmail.com>
2025-03-05 21:40:17 +0100

    Push

--- P2/config.sh
+++ P2/config.sh
@@ -2,7 +2,14 @@
 
 FILES=("host_lmataris-1" "host_lmataris-2" "router_lmataris-1" "router_lmataris-2")
 
-for FILE in "${FILES[@]}"; do
-        docker exec -it "${FILE}" ash ${FILE}
-        echo "Container ${FILE} is now configured !"
+for CONTAINER_ID in $(docker ps -q); do
+    HOSTNAME=$(docker exec "$CONTAINER_ID" hostname)
+    
+    for FILE in "${FILES[@]}"; do
+        if [ "$HOSTNAME" = "$FILE" ]; then
+            docker exec -it "$CONTAINER_ID" ash $FILE
+            echo "Container $FILE (ID: $CONTAINER_ID) is now configured!"
+            break
+        fi
+    done
 done
