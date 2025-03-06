91b305977857e82fa5d3f287d687ea9a53ffc88d
HALOis <aloislenaz@halo.fr>
2025-03-06 09:52:41 +0000

    toto

---
+++ P3/config.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+FILES=("host_lmataris-1" "host_lmataris-2" "host_lmataris-3" "router_lmataris-1" "router_lmataris-2" "router_lmataris-3" "router_lmataris-4" )
+
+for CONTAINER_ID in $(docker ps -q); do
+    HOSTNAME=$(docker exec "$CONTAINER_ID" hostname)
+    
+    for FILE in "${FILES[@]}"; do
+        if [ "$HOSTNAME" = "$FILE" ]; then
+            docker cp ${FILE} $CONTAINER_ID:/
+            docker exec -it "$CONTAINER_ID" ash $FILE
+            echo "Container $FILE (ID: $CONTAINER_ID) is now configured!"
+            break
+        fi
+    done
+done
